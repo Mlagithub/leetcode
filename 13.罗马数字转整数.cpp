9c29b8507d9399f932524a4422ca94fa361a3a52
keepone <keepone@surface>
2021-03-31 21:29:04 +0800

    romanToInt,4ms,95%, change if-else to (?:)operator

--- 13.罗马数字转整数.cpp
+++ 13.罗马数字转整数.cpp
@@ -13,14 +13,14 @@ class Solution {
 public:
 
     int romanToInt(string s) {
-        unordered_map<char, int> flags{
-            {'I', 1}, 
-            {'V', 5}, 
-            {'X', 10}, 
-            {'L', 50}, 
-            {'C', 100}, 
-            {'D', 500}, 
-            {'M', 1000}};
+        // unordered_map<char, int> flags{
+        //     {'I', 1}, 
+        //     {'V', 5}, 
+        //     {'X', 10}, 
+        //     {'L', 50}, 
+        //     {'C', 100}, 
+        //     {'D', 500}, 
+        //     {'M', 1000}};
 
         int sum = 0;
         for(int i=0; i < s.size()-1;++i)
@@ -28,13 +28,7 @@ public:
             // case 
             auto l = this->charToInt(s[i]);
             auto r = this->charToInt(s[i+1]);
-            if(l < r)
-            {
-                sum -= l;
-            }
-            else{
-                sum += l;
-            }
+            sum += (l < r) ? -l : l;
         }
         sum += this->charToInt(s[s.size()-1]);
         return sum;
