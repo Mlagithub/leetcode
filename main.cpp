7c7b53cd18946907a2bff4e35436fea537694ae6
keepone <keepone@surface>
2021-04-11 21:09:06 +0800

    70-climbStairs, 0ms, 100%

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 // #include "67.二进制求和.cpp"
-#include "69.x-的平方根.cpp"
+// #include "69.x-的平方根.cpp"
 
 // template<typename T>
 // std::ostream& operator << (std::ostream& os, std::vector<T> v)
@@ -20,7 +20,11 @@ int main(void)
 {
     Solution solution;
 
+
+
+    /* mySqrt
     std::cout << "sqrt of 2147483647 : " << solution.mySqrt(2147483647) << '\n';
+    */
 
     /* addBinary
     auto s1 = "11", s2 = "1";
