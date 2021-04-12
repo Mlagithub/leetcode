e8a9f3e5d78a43aec8a4911f9d9917b0084489bf
keepone <keepone@surface>
2021-04-12 19:14:39 +0800

    21-mergeTwoLists, 4ms, 80%

---
+++ linked-list/21.合并两个有序链表.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* rst = new ListNode();
+        ListNode* prev = rst;
+
+        while(l1!=nullptr && l2!=nullptr)
+        {
+            if(l1->val > l2->val) 
+            {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            else
+            {
+                prev->next = l1;
+                l1 = l1->next;
+            } 
+            prev = prev->next;
+        }
+        prev->next = l1 == nullptr ? l2 : l1;
+        return rst->next;
+    }
+};
+// @lc code=end
+
